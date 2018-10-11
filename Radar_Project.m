c45ea960149484224204f23e4b66806ef859f142
rroley <rroley@hmc.edu>
2018-10-11 14:11:10 -0400

    Update Radar_Project.m
    removed edits

--- Radar_Project.m
+++ Radar_Project.m
@@ -1,5 +1,5 @@
 %this is creating a document to initiate git repo
-<<<<<<< HEAD
+
 
 % Authors: 
 % Rachel Roley
@@ -7,4 +7,4 @@
 disp('start')
 =======
 disp('starting a Dragon War!')
->>>>>>> a6934ec270a84fa3b5c551a71a71897c853086a8
+
